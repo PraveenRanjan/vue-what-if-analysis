83a9e8c63f765c68396e162d23bf793670b6cceb
Praveen R <praven.ranjan@synerzip.com>
2021-02-01 22:33:50 +0530

    added vuetify

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { store } from './store/store';
-
+import Vuetify from 'vuetify'
+Vue.use(Vuetify);
 new Vue({
   el: '#app',
   store: store,
+  Vuetify,
   render: h => h(App)
 })
